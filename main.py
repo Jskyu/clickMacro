ccf5b82e0d9a20b614516faf0dcb21f1e3d149e4
Denis <52134533+Jskyu@users.noreply.github.com>
2021-09-10 10:55:53 +0900

    UPDATE 2.0

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pyautogui as auto
 import time
 
 
-VERSION = '1.21'
+VERSION = '2.0'
 
 DEFINE_X = 80
 DEFINE_Y = 50
@@ -14,8 +14,9 @@ moveY = DEFINE_Y
 print("Click Macro ver {0}".format(VERSION))
 print("https://github.com/Jskyu/clickMacro")
 print("ON/OFF : ` ")
-print("Set X : R, T")
-print("Set Y : F, G")
+print("Plus X : R,  Minus X : F")
+print("Plus Y : T,  Minus X : G")
+print("Reset X And Y : C")
 print("Excution : W")
 
 key_w = 'w'
@@ -34,121 +35,30 @@ isOn = False
 
 def pressEvent(key):
     global moveX, moveY
+    isKey = False
     
     # 가로 설정
     if key == key_r:
         moveX = moveX + 10
-
-    elif key == key_t:
-        moveX = moveX - 10
-
-        
-    # 세로 설정
+        isKey = True
     elif key == key_f:
+        moveX = moveX - 10
+        isKey = True
+
+    # 세로 설정
+    elif key == key_t:
         moveY = moveY + 8
+        isKey = True
     elif key == key_g:
         moveY = moveY - 8
+        isKey = True
     elif key == key_reset:
         moveX = DEFINE_X
         moveY = DEFINE_Y
+        isKey = True
 
-    print("Set x : {0}, y : {1}".format(moveX, moveY))
-
-
-def moveEvent():
-    x, y = auto.position()
-    auto.click()
-    tempX = x
-    tempY = y
-    print("Execution x : {0}, y : {1}".format(moveX, moveY))
-    auto.moveTo(x + moveX, y + moveY)
-    auto.click()
-    auto.moveTo(tempX, tempY)
-
-
-while True:
-    if keyboard.read_key() == key_execution:
-        if not isOn:
-            print("MACRO ON")
-            time.sleep(0.2)
-            isOn = True
-        elif isOn:
-            print("MACRO OFF")
-            time.sleep(0.2)
-            isOn = False
-
-    if keyboard.is_pressed(key_r):
-        pressEvent(key_r)
-        continue
-    if keyboard.is_pressed(key_t):
-        pressEvent(key_t)
-        continue
-    
-    if keyboard.is_pressed(key_f):
-        pressEvent(key_f)
-        continue
-    if keyboard.is_pressed(key_g):
-        pressEvent(key_g)
-        continue
-    if keyboard.is_pressed(key_w) and isOn:
-        moveEvent()
-        continue
-
-from platform import version
-import keyboard
-import pyautogui as auto
-import time
-
-
-VERSION = '1.21'
-
-DEFINE_X = 80
-DEFINE_Y = 50
-moveX = DEFINE_X
-moveY = DEFINE_Y
-
-print("Click Macro ver {0}".format(VERSION))
-print("https://github.com/Jskyu/clickMacro")
-print("ON/OFF : ` ")
-print("Set X : R, T")
-print("Set Y : F, G")
-print("Excution : W")
-
-key_w = 'w'
-
-key_r = 'r'
-key_t = 't'
-
-key_f = 'f'
-key_g = 'g'
-
-key_reset = 'c'
-
-key_execution = '`'
-
-isOn = False
-
-def pressEvent(key):
-    global moveX, moveY
-    
-    # 가로 설정
-    if key == key_r:
-        moveX = moveX + 10
-
-    elif key == key_t:
-        moveX = moveX - 10
-
-        
-    # 세로 설정
-    elif key == key_f:
-        moveY = moveY + 8
-    elif key == key_g:
-        moveY = moveY - 8
-    elif key == key_reset:
-        moveX = DEFINE_X
-        moveY = DEFINE_Y
-
-    print("Set x : {0}, y : {1}".format(moveX, moveY))
+    if isKey:
+        print("Set x : {0}, y : {1}".format(moveX, moveY))
 
 
 def moveEvent():
@@ -173,20 +83,12 @@ while True:
             print("MACRO OFF")
             time.sleep(0.2)
             isOn = False
+        continue
 
-    if keyboard.is_pressed(key_r):
-        pressEvent(key_r)
-        continue
-    if keyboard.is_pressed(key_t):
-        pressEvent(key_t)
-        continue
-    
-    if keyboard.is_pressed(key_f):
-        pressEvent(key_f)
-        continue
-    if keyboard.is_pressed(key_g):
-        pressEvent(key_g)
-        continue
-    if keyboard.is_pressed(key_w) and isOn:
+    if nowKey == key_w and isOn:
         moveEvent()
         continue
+    
+    pressEvent(nowKey)
+    time.sleep(0.1)
+    
